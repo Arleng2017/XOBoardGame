5868d41ce2047f8583f7abceace0482c3197864f
Arleng2017 <arleng2017@gmail.com>
2020-03-29 00:36:19 +0700

    cleancode and reflect

--- GameBoarder.cs
+++ GameBoarder.cs
@@ -6,20 +6,9 @@ namespace XOBoardGame
 {
     public class GameBoarder
     {
-        public Player player1;
-        public Player player2;
-        private Player theWinner { get; set; } = null;
-
-        readonly List<List<int>> WinnerCheck = new List<List<int>>() {
-            new List<int>{0,1,2 },
-            new List<int>{3,4,5 },
-            new List<int>{6,7,8 },
-            new List<int>{0,3,6 },
-            new List<int>{1,4,7 },
-            new List<int>{2,5,8 },
-            new List<int>{0,4,8 },
-            new List<int>{2,4,6 }
-         };
+        Player player1;
+        Player player2;
+        Player theWinner { get; set; } = null;
 
         static List<string> valueInBoard = new List<string>()
         {
@@ -32,7 +21,7 @@ namespace XOBoardGame
             player2 = new Player(player2Name);
         }
 
-        public static void BoardGameDisplay()
+        static void BoardGameDisplay()
         {
             int i = 0;
             foreach (var value in valueInBoard)
@@ -43,7 +32,7 @@ namespace XOBoardGame
             }
         }
 
-        public bool InputValueToBorder(int input, bool isPlayer1)
+        bool InputValueToBorder(int input, bool isPlayer1)
         {
             if (valueInBoard.ElementAt(input) != "x" && valueInBoard.ElementAt(input) != "o")
             {
@@ -55,6 +44,7 @@ namespace XOBoardGame
                 {
                     player2.inputValue(input);
                 }
+
                 valueInBoard[input] = isPlayer1 ? "x" : "o"; ;
                 return true;
             }
@@ -63,45 +53,53 @@ namespace XOBoardGame
 
         }
 
-        //public bool Gram(this Player player)
-        //{
-        //    bool haveTheWinner = false;
-        //    foreach (var winnercheck in WinnerCheck)
-        //    {
-        //        haveTheWinner = player.PlayerInput.Intersect(winnercheck).Count() == 3 ? true : false;
-        //    }
-
-        //    return haveTheWinner;
-        //}
-
-
-        public bool GameCalulate(bool isPlayer1)
+        bool GameCalulate(bool isPlayer1)
         {
             var haveTheWinner = false;
-            foreach (var check in WinnerCheck)
+            if (player1.CheckTheGameEnd() == true || player2.CheckTheGameEnd() == true)
             {
-                if (isPlayer1)
-                {
-                    if (player1.PlayerInput.Intersect(check).Count() == 3)
-                    {
-                        theWinner = player1;
-                        haveTheWinner = true;
-                        break;
-                    };
-                }
-                else
-                {
-                    if (player2.PlayerInput.Intersect(check).Count() == 3)
-                    {
-                        haveTheWinner = true;
-                        theWinner = player2;
-                        break;
-                    };
-                }
+                theWinner = isPlayer1 ? player1 : player2;
+                haveTheWinner = true;
             }
             return haveTheWinner;
         }
 
-        public Player GetTheWinner() => theWinner;
+        Player GetTheWinner() => theWinner;
+
+        public void GameStart()
+        {
+
+            bool isPlayer1;
+            GameBoarder.BoardGameDisplay();
+            int i = 0;
+            do
+            {
+                if (i % 2 == 0)
+                {
+                    Console.Write($"{player1.Name} input : ");
+                    isPlayer1 = true;
+                }
+                else
+                {
+                    Console.Write($"{player2.Name} input : ");
+                    isPlayer1 = false;
+
+                }
+
+                if (InputValueToBorder(int.Parse(Console.ReadLine()), isPlayer1))
+                {
+                    i++;
+                };
+
+                Console.WriteLine();
+                GameBoarder.BoardGameDisplay();
+                if (GameCalulate(isPlayer1))
+                {
+                    Console.WriteLine("the winner is " + GetTheWinner().Name);
+                    break;
+                };
+            } while (!GameCalulate(isPlayer1) && i < 9);
+        }
+
     }
 }
---
+++ GameCompute.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace XOBoardGame
+{
+    public static class GameCompute
+    {
+        static readonly List<List<int>> WinnerCheck = new List<List<int>>() {
+            new List<int>{0,1,2 },
+            new List<int>{3,4,5 },
+            new List<int>{6,7,8 },
+            new List<int>{0,3,6 },
+            new List<int>{1,4,7 },
+            new List<int>{2,5,8 },
+            new List<int>{0,4,8 },
+            new List<int>{2,4,6 }
+         };
+
+        public static bool CheckTheGameEnd(this Player player)
+        {
+            bool haveTheWinner = false;
+            foreach (var winnercheck in WinnerCheck)
+            {
+                if (!haveTheWinner)
+                {
+                    haveTheWinner = player.PlayerInput.Intersect(winnercheck).Count() == 3 ? true : false;
+                }
+                else
+                    break;
+            }
+
+            return haveTheWinner;
+        }
+
+    }
+}
